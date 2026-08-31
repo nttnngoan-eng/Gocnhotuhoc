3155cb3dff7973c88c5a6417eb498d29a06f8dce
nttnngoan-eng <nttn.ngoan@gmail.com>
2026-08-31 10:34:57 +0700

    Add files via upload

---
+++ catalog.js
@@ -0,0 +1,21 @@
+window.GNTT_CATALOG = {
+  "books": [
+    {
+      "id": "a-ti-dat-ma-phap-uan-tuc-luan",
+      "title": "A-TI-ĐẠT-MA PHÁP UẨN TÚC LUẬN",
+      "description": "Tài liệu học và bài giảng được sắp xếp theo phẩm/chương.",
+      "chapters": [
+        {
+          "title": "PHẨM 12: VÔ LƯỢNG",
+          "lessons": [
+            {
+              "title": "BÀI 40 - TỨ VÔ LƯỢNG",
+              "subtitle": "Giảng thọ sư: TT Thích Huyền Châu",
+              "href": "reader.html?v=19"
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
