897da5000723a5c0d4de3a9c8fb2690e6e421856
nttnngoan-eng <nttn.ngoan@gmail.com>
2026-09-01 06:45:04 +0700

    Cập nhật: BÀI 40 - TỪ VÔ LƯỢNG - danh mục

--- catalog.js
+++ catalog.js
@@ -4,6 +4,9 @@ window.GNTT_CATALOG = {
       "id": "a-ti-dat-ma-phap-uan-tuc-luan",
       "title": "A-TI-ĐẠT-MA PHÁP UẨN TÚC LUẬN",
       "description": "Tài liệu học và bài giảng được sắp xếp theo phẩm/chương.",
+      "type": "lesson",
+      "pdfUrl": "",
+      "author": "",
       "chapters": [
         {
           "id": "pham-12-vo-luong",
@@ -13,6 +16,7 @@ window.GNTT_CATALOG = {
               "id": "a-ti-dat-ma-phap-uan-tuc-luan-pham-12-vo-luong-bai-40",
               "title": "BÀI 40 - TỪ VÔ LƯỢNG",
               "subtitle": "Giảng thọ sư: TT Thích Huyền Châu",
+              "tocLevel": 2,
               "href": "reader.html?id=a-ti-dat-ma-phap-uan-tuc-luan-pham-12-vo-luong-bai-40&v=23"
             }
           ]
